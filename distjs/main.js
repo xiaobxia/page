afa947008b890f27413c8efb12f5618f2ea0ef79
chenlingjie <chenlingjie@cd121.com>
2017-09-01 18:10:08 +0800

    删除意外文件

--- distjs/main.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by xiaobxia on 2017/9/1.
- */
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogQ3JlYXRlZCBieSB4aWFvYnhpYSBvbiAyMDE3LzkvMS5cbiAqL1xuIl19
